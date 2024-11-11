5f87be08aa416482bdae9861666c773ae882b778
Kevin Liang <kevinliang22069@gmail.com>
2024-11-10 22:19:56 -0500

    completed backprop functions

--- Layers/ConvL/ConvBackPropagation.cpp
+++ Layers/ConvL/ConvBackPropagation.cpp
@@ -80,6 +80,27 @@ void ConvL::add_gradient_filters() {
     }
 }
 
+//for each output channel, convolve each channel of the 3d filter tensor, sum each slice, average
 void ConvL::calc_gradient_feature_map() {
+    int input_channels = feature_map.dimension(2);
+    int output_channels = gradient_unpooled.dimension(2);
+
+    assert(input_channels == filter.dimension(2));
+    assert(output_channels == filter.dimension(3));
+
+    gradient_feature_map.reshape(feature_map.dimensions());
     gradient_feature_map.setZero();
+
+    for (int input_channel = 0; input_channel < input_channels; ++input_channel) {
+
+        Tensor<double, 2> input_slice = gradient_feature_map.chip(input_channel, 2);
+        for (int output_channel = 0; output_channel < output_channels; ++output_channel) {
+            Tensor<double, 2> output_slice = gradient_unpooled.chip(output_channel, 2).
+                    reverse(Eigen::array<int, 2>({1, 0}));
+            Tensor<double, 2> filter_slice = filter.chip(output_channel, 3).chip(input_channel, 2);
+            convolve_full(filter_slice, output_slice, input_slice);
+        }
+
+        input_slice = input_slice / static_cast<double>(output_channels);
+    }
 }
